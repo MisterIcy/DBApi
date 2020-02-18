8b64ebfd40dedee4f73606c86dea828a5197b5f1
Alexandros Koutroulis <icyd3mon@gmail.com>
2020-02-18 11:32:30 +0200

    Modified Entity Manager to Resolve a problem where an Object cannot be Cast to T

--- IEntityManager.cs
+++ IEntityManager.cs
@@ -17,7 +17,7 @@ namespace DBApi
         object FindById(Type entityType, object identifier);
         T FindOneBy<T>(Dictionary<string, object> parameters) where T : class;
         object FindOneBy(Type entityType, Dictionary<string, object> parameters);
-        List<T> FindBy<T>(Dictionary<string, object> parameters);
+        List<T> FindBy<T>(Dictionary<string, object> parameters) where T: class;
         List<object> FindBy(Type entityType, Dictionary<string, object> parameters);
         List<T> FindAll<T>() where T: class;
         List<object> FindAll(Type entityType);
