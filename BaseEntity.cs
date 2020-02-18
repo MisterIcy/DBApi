b66c0f179cf3a6e7b17883ecddaa5bb21afd874a
Alexandros Koutroulis <icyd3mon@gmail.com>
2020-02-18 11:32:10 +0200

    Minor Modifications Added BaseEntity and IQuery

---
+++ BaseEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DBApi.Entities
+{
+    /// <summary>
+    /// Κλάση η οποία κληρονομείται από τα entities, έτσι ώστε να υπάρχει ένα 
+    /// κοινό API, όσον αφορά το development των παρεκλομένων υπηρεσιών
+    /// </summary>
+    /// <remarks></remarks>
+    public abstract class BaseEntity : INotifyPropertyChanged
+    {
+        #region Events
+        /// <summary>
+        /// Event το οποίο πυροδοτείται όταν αλλάζει ένα property
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+
+        public bool Loaded { get; set; } = false;
+        public bool Incomplete { get; set; } = false;
+    }
+}
---
+++ IQuery.cs
@@ -0,0 +1,46 @@
+﻿using DBApi.Reflection;
+using System;
+using System.Collections.Generic;
+using DBApi.QueryBuilder;
+using System.Text;
+
+namespace DBApi
+{
+    public interface IQuery
+    {
+        /// <summary>
+        /// Returns the Sql Server Query
+        /// </summary>
+        /// <returns></returns>
+        string GetNativeQuery();
+
+        IQuery Select(ClassMetadata metadata);
+        IQuery Select(params string[] fields);
+        IQuery Select(string expression);
+        IQuery From(ClassMetadata metadata, string alias = null);
+        IQuery From(string tableName, string alias = null);
+        IQuery From(IQuery internalQuery, string alias = null);
+        IQuery Where(Expression expression);
+        IQuery AndWhere(Expression expression);
+        IQuery OrWhere(Expression expression);
+        IQuery OrderBy(string expression);
+        IQuery OrderBy(string field, string order);
+        IQuery Join(string tableName, string tableAlias, Expression on);
+        IQuery InnerJoin(string tableName, string tableAlias, Expression on);
+        IQuery LeftJoin(string tableName, string tableAlias, Expression on);
+        IQuery RightJoin(string tableName, string tableAlias, Expression on);
+        IQuery GroupBy(string field);
+        IQuery Having(Expression expression);
+        IQuery Like(string field, object term);
+        IQuery Top(int rows, bool percentage = false);
+        IQuery Insert(ClassMetadata metadata);
+        IQuery Insert(string tableName, params string[] fields);
+        IQuery Update(ClassMetadata metadata);
+        IQuery Update(string tableName, params string[] fields);
+        IQuery Delete(ClassMetadata metadata);
+        IQuery BeginTransaction();
+        IQuery Commit();
+        IQuery Rollback();
+
+    }
+}
