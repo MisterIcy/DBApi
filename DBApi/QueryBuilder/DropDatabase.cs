895cc8f8c83f95d8c59f6063b5807921cb8bedfc
Alexandros Koutroulis <icyd3mon@gmail.com>
2021-01-18 21:43:36 +0200

    Added DropDatabase expression

---
+++ DBApi/QueryBuilder/DropDatabase.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="DropDatabase.cs">
+// Copyright 2020 Alexandros Koutroulis <icyd3mon@gmail.com>
+// This file is part of DBApi
+// DBApi is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// DBApi is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with DBApi. If not, see http://www.gnu.org/licenses/.
+// </copyright>
+// <author>Alexandros Koutroulis</author>
+// <date>2021-01-18</date>
+
+using System;
+using DBApi.Annotations;
+
+namespace DBApi.QueryBuilder
+{
+    public class DropDatabase : Expression
+    {
+        [PublicAPI]
+        public string DatabaseName { get; protected set; }
+        
+        public DropDatabase(string database) : base(-200)
+        {
+            DatabaseName = string.IsNullOrEmpty(database)
+                ? throw new ArgumentNullException(nameof(database))
+                : database;
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return "DROP DATABASE " + DatabaseName;
+        }
+    }
+}
